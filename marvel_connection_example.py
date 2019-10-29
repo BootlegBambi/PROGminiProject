cf93121a428c3362bdfe00367d3b70e89abc05fd
Altijd_Youri <advertentiey@vanderstar.eu>
2019-10-29 12:40:19 +0100

    .

--- marvel_connection_example.py
+++ marvel_connection_example.py
@@ -1,8 +1,18 @@
 import marvel_conn
-
+'''
 data = marvel_conn.get_data("/v1/public/characters", {'limit': '10', 'offset': '20'})
 
 if data:
     print(data)
+else:
+    print("Failed to get data!")
+'''
+# 1493 charachters
+# off = rand( 0-(total-1) )
+# character = marvel_conn.get_data("/v1/public/characters", {'offset': off, 'limit': '1'})
+
+character = marvel_conn.get_data("/v1/public/characters", {'offset': '200', 'limit': '1'})
+if character:
+    print(character)
 else:
     print("Failed to get data!")
