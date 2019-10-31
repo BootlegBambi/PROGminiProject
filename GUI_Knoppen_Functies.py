768ae745d58d48b0c2d4add2db046bb4a6f5f13a
michielslagboom <57129408+michielslagboom@users.noreply.github.com>
2019-10-31 13:51:11 +0100

    updateversie GUI

---
+++ GUI_Knoppen_Functies.py
@@ -0,0 +1,9 @@
+def buttonSuperheld():
+    #code voor ophalen naam uit marvelAPI
+    return 'random superheld'
+
+def buttonAntwoord():
+    #code voor ophalen goeie superheld
+    return 'goeie superheld'
+
+#testen met functies voor de antwoorden
